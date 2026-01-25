ce2d5f8ab3094af39920b1f1a500de4a5b79745c
student-nirajkumar <neerajkumar89212@gmail.com>
2026-01-25 14:25:37 +0530

    Understand about Util code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-//require('dotenv') .config ({path: './env'})
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-dotenv.config({
-    path: '.env'
-})
+
+dotenv.config({ path: ".env" });
+
+const app = express();
 
 connectDB()
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is running at port : ${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed", err);
+  });
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiRespone {
+    constructor(statusCode, data, message="sucess"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.sucess = statusCode < 400 
+    }
+}
+export {ApiRespone}
